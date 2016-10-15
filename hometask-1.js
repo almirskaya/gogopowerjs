c49bb0a56d195a9ac49d93dbaf240b56878b0937
almirskaya <alexamirskaya@gmail.com>
2016-10-15 12:02:00 +0300

    Update hometask-1.js
    average values of odd&even have been added

--- hometask-1.js
+++ hometask-1.js
@@ -12,9 +12,13 @@ var changedEvenValues=evenValuesFibonachchi.map(x=>x-2);
 var resultChangedEvenValues=changedEvenValues.reduce(function getSum(sum,current){return sum+current});
 var changedOddValues=oddValuesFibonachchi.map(x=>x-4);
 var resultChangedOddValues=changedOddValues.reduce(function getSum(sum,current){return sum+current});
+var averageResultOdd=resultChangedOddValues/(oddValuesFibonachchi.length+1);
+var averageResultEven=resultChangedEvenValues/(evenValuesFibonachchi.length+1);
 console.log(oddValuesFibonachchi);
 console.log(evenValuesFibonachchi);
 console.log(changedEvenValues);
 console.log(changedOddValues);
 console.log(resultChangedOddValues);
 console.log(resultChangedEvenValues);
+console.log(averageResultOdd);
+console.log(averageResultEven)
