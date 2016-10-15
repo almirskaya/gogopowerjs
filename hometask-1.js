ea8a2589113eaca161c505e404a50754f8620b0a
almirskaya <alexamirskaya@gmail.com>
2016-10-15 03:24:37 +0300

    Add files via upload
    array Fibonachchi was founded by JS

---
+++ hometask-1.js
@@ -0,0 +1,20 @@
+var arrayNumbers=[1,1];
+function getFibonachchiArray(n){
+        for (var i=2;i<n;i++){
+            arrayNumbers[i]=arrayNumbers[i-1]+arrayNumbers[i-2];
+    }
+    return arrayNumbers;
+}
+getFibonachchiArray(40);
+var evenValuesFibonachchi = arrayNumbers.filter(x => x%2===0);
+var oddValuesFibonachchi = arrayNumbers.filter(x => x%2!=0);
+var changedEvenValues=evenValuesFibonachchi.map(x=>x-2);
+var resultChangedEvenValues=changedEvenValues.reduce(function getSum(sum,current){return sum+current});
+var changedOddValues=oddValuesFibonachchi.map(x=>x-4);
+var resultChangedOddValues=changedOddValues.reduce(function getSum(sum,current){return sum+current});
+console.log(oddValuesFibonachchi);
+console.log(evenValuesFibonachchi);
+console.log(changedEvenValues);
+console.log(changedOddValues);
+console.log(resultChangedOddValues);
+console.log(resultChangedEvenValues);
