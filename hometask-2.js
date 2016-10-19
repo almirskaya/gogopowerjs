e6c4b284491440e1c4b69b63f50aa81ea10063a1
almirskaya <alexamirskaya@gmail.com>
2016-10-19 10:18:13 +0300

    function isPowerOfTwo
    remake of function isPowerOfTwo, now this function is  recursive function

--- hometask-2.js
+++ hometask-2.js
@@ -1,9 +1,13 @@
-function isPowerOfTwo(verificationNumber){
-    if ((Math.pow(2,Math.log2(verificationNumber)))===verificationNumber){
+function isPowerOfTwo(verificationNumber, baseOfVerNumber){
+    var baseOfVerNumber=2;
+    var interjacentNumber=verificationNumber/baseOfVerNumber;
+    if (interjacentNumber!=2 && interjacentNumber>2){
+        return isPowerOfTwo(verificationNumber-interjacentNumber);
+    }
+    else if (interjacentNumber===2) {
         return true;
     }
     else {
         return false;
     }
-
 }
